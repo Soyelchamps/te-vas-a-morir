2104b9460076351cd7f04613d7e7f42f5fc67528
Carlos Diaz <92885752+Soyelchamps@users.noreply.github.com>
2023-03-15 20:34:53 -0600

    Complete

--- src/components/Credits/Credits.jsx
+++ src/components/Credits/Credits.jsx
@@ -1,8 +1,22 @@
 const Credits = () => {
   return (
-    <div className="mt-4 text-center ">
-      <h5>Carlos Diaz</h5>
-      <h6>Web developer and Experience creator</h6>
+    <div className="flex items-center justify-between p-3 border-b border-solid border-gray-600">
+      <a href="https://www.linkedin.com/in/carlos-diazbarriga/">
+        <img src="/linkedin.svg" alt="LinkedIn" />
+      </a>
+      <div className="mt-4 text-center ">
+        <h5>Carlos Diaz</h5>
+        <h6>Web developer and Experience creator</h6>
+      </div>
+      {/* <a href="https://github.com/Soyelchamps/">
+        <video autoplay loop muted>
+          <source src="/github-gif.gif" type="video/gif" />
+        </video>
+      </a> */}
+
+      <a href="https://github.com/Soyelchamps/">
+        <img src="/github.svg" alt="Github" />
+      </a>
     </div>
   );
 };
--- src/components/LifeExpectancyCounter/LifeExpectancyCounter.jsx
+++ src/components/LifeExpectancyCounter/LifeExpectancyCounter.jsx
@@ -36,7 +36,7 @@ function LifeExpectancyCounter() {
       numminutes +
       " minutos " +
       numseconds +
-      " segundos"
+      "  seg"
     );
   }
 
